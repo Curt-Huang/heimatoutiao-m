3e50cdcfab8a317f2003249e1744b5cebc3c3f1e
Curt-Huang <3062344789@qq.com>
2020-02-05 23:09:44 +0800

    登录表单校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,10 +3,10 @@
     <van-nav-bar title="登录" left-arrow @click-left="$router.back()"></van-nav-bar>
     <!-- 手机号  验证码  登录按钮 -->
     <van-cell-group>
-      <van-field  label="手机号" placeholder="请输入手机号"></van-field>
-      <van-field  label="验证码" placeholder="请输入验证码">
+      <van-field :error-message="errMsg.mobile" @blur="checkMobile" label="手机号" placeholder="请输入手机号" v-model="loginForm.mobile"></van-field>
+      <van-field :error-message="errMsg.code" @blur="checkCode"  label="验证码" placeholder="请输入验证码" v-model="loginForm.code">
         <!-- slot指定给哪个坑填内容 -->
-        <van-button  slot="button" size="small" type="primary">发送验证码</van-button>
+        <van-button  slot="button" size="small" type="primary" >发送验证码</van-button>
       </van-field>
     </van-cell-group>
     <div class='btn-box'>
@@ -17,7 +17,56 @@
 
 <script>
 export default {
-  name: 'login'
+  name: 'login',
+  data () {
+    return {
+      loginForm: {
+        mobile: '13911111111',
+        code: '246810'
+      },
+      errMsg: {
+        mobile: '',
+        code: ''
+      }
+    }
+  },
+  methods: {
+    // 检查输入的手机格式
+    checkMobile () {
+      if (!this.loginForm.mobile) {
+        this.errMsg.mobile = '手机号不能为空'
+        return false
+      }
+      if (!/^1[3-9]\d{9}$/.test(this.loginForm.mobile)) {
+        this.errMsg.mobile = '请输入正确的手机格式'
+        return false
+      }
+      // 清空提示信息
+      this.errMsg.mobile = ''
+      return true
+    },
+    // 检查验证码格式
+    checkCode () {
+      if (!this.loginForm.code) {
+        this.errMsg.code = '验证码不能为空'
+        return false
+      }
+      if (!/^\d{6}$/.test(this.loginForm.code)) {
+        this.errMsg.code = '验证码格式不正确'
+        return false
+      }
+      // 清空提示信息
+      this.errMsg.code = ''
+      return true
+    },
+    // 登录方法
+    login () {
+      // 如果表单验证都通过，就可以向后台发送登录请求了
+      if (this.checkMobile && this.checkCode) {
+
+      }
+    }
+  }
 }
 </script>
 
